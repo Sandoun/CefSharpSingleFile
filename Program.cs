e39c108a52be6c3aeba122ecfdc8420c42f9b90b
Sandoun <felixweiss@mdg.email>
2022-04-05 23:14:28 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestCef {
+    class Program {
+
+        [STAThread]
+        static int Main (string[] args) {
+
+            //To support High DPI this must be before CefSharp.BrowserSubprocess.SelfHost.Main so the BrowserSubprocess is DPI Aware
+            CefSharp.Cef.EnableHighDPISupport();
+
+            var exitCode = CefSharp.BrowserSubprocess.SelfHost.Main(args);
+
+            if (exitCode >= 0) {
+                return exitCode;
+            }
+
+            var settings = new CefSettings() {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache"),
+                BrowserSubprocessPath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName
+            };
+
+            //NOTE: WebRTC Device Id's aren't persisted as they are in Chrome see https://bitbucket.org/chromiumembedded/cef/issues/2064/persist-webrtc-deviceids-across-restart
+            settings.CefCommandLineArgs.Add("enable-media-stream");
+            //https://peter.sh/experiments/chromium-command-line-switches/#use-fake-ui-for-media-stream
+            settings.CefCommandLineArgs.Add("use-fake-ui-for-media-stream");
+            //For screen sharing add (see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access#comment-58677180)
+            settings.CefCommandLineArgs.Add("enable-usermedia-screen-capturing");
+
+            CefSharp.Cef.Initialize(settings, performDependencyCheck: false);
+
+            var app = new App();
+            app.InitializeComponent();
+            return app.Run();
+
+        }
+
+    }
+}
